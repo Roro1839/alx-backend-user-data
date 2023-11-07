ec2a49b57cb96d5f088938c60dbcedb07da5c008
Roro1839 <ronaldokipkirui90@gmail.com>
2023-11-07 22:25:59 +0300

    updated

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,5 +3,19 @@
 """
 from api.v1.auth.auth import Auth
 
+
 class BasicAuth(Auth):
     """ Basic Authentication """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Return the Base64"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        temp = authorization_header.split(" ")[-1]
+        return temp
