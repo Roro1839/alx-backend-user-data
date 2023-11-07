37a230e56482442a1d9b71b32b0a3de566cdf243
Roro1839 <ronaldokipkirui90@gmail.com>
2023-11-07 19:21:05 +0300

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,15 +3,27 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """Authorize """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require auth"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/'):
+                excluded_path = excluded_path[:-1]
+            if path.startswith(excluded_path):
+                return False
+        return True
+
     def authorization_header(self, request=None) -> str:
         """Authorize header"""
         return None
+
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user """
         return None
-    
