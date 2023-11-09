8d1fd97ac998229119fb2997ffad7d08b61ecae6
Roro1839 <ronaldokipkirui90@gmail.com>
2023-11-09 12:37:08 +0300

    users

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -34,14 +34,14 @@ def before_request():
 
     allowed_paths = [
             '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if request.path in allowed_paths:
-        return
+    if auth.require_auth(request.path, allowed_paths):
 
-    if auth.authorization_header(request) is None:
-        abort(401)
+        if auth.authorization_header(request) is None:
+            abort(401)
 
-    if auth.current_user(request) is None:
-        abort(403)
+        if auth.current_user(request) is None:
+            abort(403)
+    return
 
 
 @app.errorhandler(404)
