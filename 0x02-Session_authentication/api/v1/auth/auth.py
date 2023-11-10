d0c03083e687b7db5ae2b7f43ed67a508c73754f
Roro1839 <ronaldokipkirui90@gmail.com>
2023-11-10 03:15:42 +0300

    users

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Module for auth.py"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Authorize """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require auth"""
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/'):
+                excluded_path = excluded_path[:-1]
+            if path.startswith(excluded_path):
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Handles authentication requests
+        """
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        return header if header else None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user """
+        return None
